f3c3f650570c8c7760413a42b9230340c2bb2e97
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-19 16:19:56 +0100

    ADD: secondo esercizio lista studenti

--- app.js
+++ app.js
@@ -14,18 +14,111 @@ const nomiOspiti = [
 ];
 
 const tavolo = 'Tavolo VIP';
-let Partecipante;
 let postoTavolo = 1;
 
 
 for (let i = 0; i < nomiOspiti.length; i++){
-    Partecipante = nomiOspiti[i];
+    let partecipante = nomiOspiti[i];
 
     const oggetto = {
         nomeTavolo: tavolo,
-        nomeOspite: Partecipante,
+        nomeOspite: partecipante,
         posto: postoTavolo++
     }
     
     console.log(oggetto);
 };
+
+// nomiOspiti.forEach(function (partecipante) {
+//     const oggetto = {
+//         nomeTavolo: tavolo,
+//         nomeOspite: partecipante,
+//         posto: postoTavolo++
+//     }
+    
+//     console.log(oggetto);
+// });
+
+// nomiOspiti.forEach((partecipante) => {
+//     const oggetto = {
+//         nomeTavolo: tavolo,
+//         nomeOspite: partecipante,
+//         posto: postoTavolo++
+//     }
+    
+//     console.log(oggetto);
+// });
+
+// il metodo map restituisce un array
+
+// const guestArray = nomiOspiti.map((partecipante) => {
+//     const oggetto = {
+//         nomeTavolo: tavolo,
+//         nomeOspite: partecipante,
+//         posto: postoTavolo++
+//     }
+
+//     return oggetto;
+// });
+
+// console.log(guestArray);
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+const studenti = [
+    {
+        id: 213, 
+        nome: 'Marco della Rovere',
+        grado: 78,
+    }, 
+
+    {
+        id: 110,
+        nome: 'Paola Cortellessa',
+        grado: 96,
+    }, 
+
+    {
+        id: 250, 
+        nome: 'Andrea Mantegna',
+        grado: 48,
+    }, 
+
+    {
+        id: 145, 
+        nome: 'Gaia Borromini',
+        grado: 74,
+    }, 
+
+    {
+        id: 196, 
+        nome: 'Luigi Grimaldello',
+        grado: 68,
+    }, 
+
+    {
+        id: 102, 
+        nome: 'Piero della Francesca',
+        grado: 50,
+    }, 
+
+    {
+        id: 120, 
+        nome: 'Francesca da Polenta',
+        grado: 84,
+    }, 
+];
+
+const nomiMaiuscoli = studenti.map((elemento) =>{
+    return elemento.nome.toUpperCase();
+});
+
+console.log(nomiMaiuscoli);
+
+let studentiSecchioni = studenti.filter((el) => el.grado > 70);
+let studentiSecchioniId = studenti.filter((el) => el.grado > 70 && el.id > 120);
+
+console.log(studentiSecchioni);
+console.log(studentiSecchioniId);
